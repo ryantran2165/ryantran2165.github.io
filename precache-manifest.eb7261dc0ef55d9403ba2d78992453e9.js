f5f62b8715ea20c4ba55085c48f0a394fbf38ee0
ryan_tran <ryantran2165@gmail.com>
2021-01-12 11:09:39 -0800

    Update descriptions and skills

---
+++ precache-manifest.eb7261dc0ef55d9403ba2d78992453e9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85129be8be1261e849c6a004f818f431",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4834bcdf0bd18947621a",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "5c4d64a78187fa57e3f5",
+    "url": "/static/css/main.58e35dfb.chunk.css"
+  },
+  {
+    "revision": "4834bcdf0bd18947621a",
+    "url": "/static/js/2.b30ef120.chunk.js"
+  },
+  {
+    "revision": "c2e5adf729da2dc21fde87ed8368eb0b",
+    "url": "/static/js/2.b30ef120.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c4d64a78187fa57e3f5",
+    "url": "/static/js/main.0a459e44.chunk.js"
+  },
+  {
+    "revision": "8db387158b95a1897788",
+    "url": "/static/js/runtime-main.8253bd70.js"
+  },
+  {
+    "revision": "feb58fdd794675730a2dd0aa80ac379c",
+    "url": "/static/media/about_background.feb58fdd.jpg"
+  },
+  {
+    "revision": "32e692b80c4ad5b1ca1c6361073b44ba",
+    "url": "/static/media/bjj_predictor.32e692b8.jpg"
+  },
+  {
+    "revision": "fd083156405c1333b1aa82ef4e54e6c7",
+    "url": "/static/media/connect4_ai.fd083156.jpg"
+  },
+  {
+    "revision": "6537d22be14e27a3068a9449e023a00c",
+    "url": "/static/media/crypto_online.6537d22b.jpg"
+  },
+  {
+    "revision": "fdbfa13ea1cad6d84d23ee40497cf3ab",
+    "url": "/static/media/digit_recognizer.fdbfa13e.jpg"
+  },
+  {
+    "revision": "922e5417c1dde81e25fef2797ecddd80",
+    "url": "/static/media/donut_dash.922e5417.jpg"
+  },
+  {
+    "revision": "edbbe177ba9678c0f4d5b75f6a3d21a7",
+    "url": "/static/media/favicon.edbbe177.jpg"
+  },
+  {
+    "revision": "822ded7ea7069c59d670f59489cc9068",
+    "url": "/static/media/infinite_monkey.822ded7e.jpg"
+  },
+  {
+    "revision": "2c5b6b2bb48d16ddeda7d12cf35a0407",
+    "url": "/static/media/office_supply_depot.2c5b6b2b.jpg"
+  },
+  {
+    "revision": "7c44bf6d3e36579a52c509775110a977",
+    "url": "/static/media/polynomial_regression.7c44bf6d.jpg"
+  },
+  {
+    "revision": "3d447519a8b5e06ecebeb4da4384b485",
+    "url": "/static/media/ryan_tran.3d447519.jpg"
+  },
+  {
+    "revision": "585f243b032ce5c983595eb79497953b",
+    "url": "/static/media/suzanne_nguyen.585f243b.jpg"
+  },
+  {
+    "revision": "9ff1bf2d6ea00214a8b071d9aed03d24",
+    "url": "/static/media/under_construction.9ff1bf2d.jpg"
+  }
+]);

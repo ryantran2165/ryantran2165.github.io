89f89c8d7c95c4bb4c6aa60fd737074687e39521
Ryan Tran <ryantran2165@gmail.com>
2024-12-18 18:02:59 -0800

    Animate skill circles individually

--- src/components/skills/skill-circle.js
+++ src/components/skills/skill-circle.js
@@ -1,17 +1,55 @@
+import ProgressBar from "progressbar.js/dist/progressbar";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
 import Col from "react-bootstrap/Col";
+import { Waypoint } from "react-waypoint";
 
-const SkillCircle = ({ id }) => {
+const SkillCircle = ({ language }) => {
+  const [animated, setAnimated] = useState(false);
+
+  function animate() {
+    if (animated) {
+      return;
+    }
+
+    setAnimated(true);
+
+    var circle = new ProgressBar.Circle(`#${language.id}`, {
+      color: "#222",
+      strokeWidth: 3,
+      trailColor: "#eee",
+      trailWidth: 1,
+      easing: "easeInOut",
+      duration: 2000,
+      from: { color: "#7fffd4" },
+      to: { color: "#7fffd4" },
+
+      step: function (state, circle) {
+        circle.path.setAttribute("stroke", state.color);
+
+        var value = Math.round(circle.value() * 5);
+        if (value === 0) {
+          circle.setText("");
+        } else {
+          circle.setText(value + "<br>" + language.display);
+        }
+      },
+    });
+
+    circle.animate(language.rating / 5);
+  }
+
   return (
-    <Col xs="auto" className="mt-3 mx-2" data-aos="fade-in" data-aos-delay="200">
-      <div id={id} className="skillCircle"></div>
-    </Col>
+    <Waypoint onEnter={animate}>
+      <Col xs="auto" className="mt-3 mx-2" data-aos="fade-in" data-aos-delay="200">
+        <div id={language.id} className="skillCircle"></div>
+      </Col>
+    </Waypoint>
   );
 };
 
 SkillCircle.propTypes = {
-  id: PropTypes.string,
+  language: PropTypes.object,
 };
 
 export default SkillCircle;
--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -1,9 +1,8 @@
-import ProgressBar from "progressbar.js/dist/progressbar";
 import React, { Component } from "react";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import { Waypoint } from "react-waypoint";
+
 import {
   FRAMEWORKS_LIBRARIES,
   MACHINE_LEARNING,
@@ -15,73 +14,31 @@ import SkillCircle from "./skill-circle";
 import SkillList from "./skill-list";
 
 class Skills extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { skillsAnimated: false };
-  }
-
-  animateSkills = () => {
-    if (!this.state.skillsAnimated) {
-      for (const language in PROGRAMMING_LANGUAGES) {
-        const lang = PROGRAMMING_LANGUAGES[language];
-        createSkill(`#${lang.id}`, lang.display, lang.rating / 5);
-      }
-      this.setState({ skillsAnimated: true });
-    }
-
-    function createSkill(container, skill, percent) {
-      var circle = new ProgressBar.Circle(container, {
-        color: "#222",
-        strokeWidth: 3,
-        trailColor: "#eee",
-        trailWidth: 1,
-        easing: "easeInOut",
-        duration: 2000,
-        from: { color: "#7fffd4" },
-        to: { color: "#7fffd4" },
-
-        step: function (state, circle) {
-          circle.path.setAttribute("stroke", state.color);
-
-          var value = Math.round(circle.value() * 5);
-          if (value === 0) {
-            circle.setText("");
-          } else {
-            circle.setText(value + "<br>" + skill);
-          }
-        },
-      });
-      circle.animate(percent);
-    }
-  };
-
   render() {
     return (
-      <Waypoint onEnter={this.animateSkills}>
-        <Container id="skills" fluid className="text-center bg-white py-5">
-          <SectionTitle text="Skills" />
-          <Row className="justify-content-center" data-aos="fade-in" data-aos-delay="200">
-            <Col xs="auto">3 - Proficient</Col>
-            <Col xs="auto">4 - Advanced</Col>
-            <Col xs="auto">5 - Expert</Col>
-          </Row>
-          <Row className="mt-4" data-aos="fade-in" data-aos-delay="200">
-            <Col>
-              <h4 className="font-weight-bold">Languages</h4>
-            </Col>
-          </Row>
-          <Row className="justify-content-center">
-            {Object.entries(PROGRAMMING_LANGUAGES).map((pair) => (
-              <SkillCircle id={pair[1].id} key={`${pair[0]}-circle`} />
-            ))}
-          </Row>
-          <Row className="justify-content-evenly mx-2">
-            <SkillList subheading="Frameworks/Libraries" skills={FRAMEWORKS_LIBRARIES} />
-            <SkillList subheading="Tools" skills={TOOLS} />
-            <SkillList subheading="Machine Learning" skills={MACHINE_LEARNING} />
-          </Row>
-        </Container>
-      </Waypoint>
+      <Container id="skills" fluid className="text-center bg-white py-5">
+        <SectionTitle text="Skills" />
+        <Row className="justify-content-center" data-aos="fade-in" data-aos-delay="200">
+          <Col xs="auto">3 - Proficient</Col>
+          <Col xs="auto">4 - Advanced</Col>
+          <Col xs="auto">5 - Expert</Col>
+        </Row>
+        <Row className="mt-4" data-aos="fade-in" data-aos-delay="200">
+          <Col>
+            <h4 className="font-weight-bold">Languages</h4>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          {Object.values(PROGRAMMING_LANGUAGES).map((language) => (
+            <SkillCircle language={language} key={`${language.id}-circle`} />
+          ))}
+        </Row>
+        <Row className="justify-content-evenly mx-2">
+          <SkillList subheading="Frameworks/Libraries" skills={FRAMEWORKS_LIBRARIES} />
+          <SkillList subheading="Tools" skills={TOOLS} />
+          <SkillList subheading="Machine Learning" skills={MACHINE_LEARNING} />
+        </Row>
+      </Container>
     );
   }
 }

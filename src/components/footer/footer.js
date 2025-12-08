7341f59ca2e1176a6427ff78b58e0ea8b7dfd50d
Ryan Tran <ryantran2165@gmail.com>
2025-12-07 20:55:13 -0800

    Fix typo

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer data-aos="fade-in" data-aos-delay="200" data-aos-offset="-10">
       <Container fluid className="py-3">
-        <h4 className="text-center m-0">Thanks for visting!</h4>
+        <h4 className="text-center m-0">Thanks for visiting!</h4>
       </Container>
     </footer>
   );
